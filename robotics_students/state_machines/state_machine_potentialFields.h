b135b6e8fce7c06a958d822553e2f820f858d4f6
VanessaHedz <82928993+VanessaHedz@users.noreply.github.com>
2024-03-30 11:09:58 -0600

    Funciones para el cálculo del punto.

--- robotics_students/state_machines/state_machine_potentialFields.h
+++ robotics_students/state_machines/state_machine_potentialFields.h
@@ -8,8 +8,94 @@
  *                                                      *
  ********************************************************/
 
- // Student State Machine 
-AdvanceAngle potential_fields(Raw observations, int dest, int intensity, float Mag_Advance, float max_angle, int num_sensors){
+//* -------------------------------- FUNCTIONS -------------------------------- *//
+// En utilities/utilities.h
+    // dif_vectors(coord vector1, coord vector2)        <- Resta de 2 puntos (vector 1 - vector 2)
+    // magnitude(coord vector)                          <- Saca la magnitud de un vector 
+    // distance (coord vector1, coord vector2)          <- Obtiene la distancia entre un punto y otro.
+    // divide_vector_scalar(coord vector1,float cnt)    <- Divide un vector entre un escalar :p
+
+//Funciones propias
+    //Suma de 2 puntos
+coord sum_vectors(coord vector1,coord vector2){
+
+    coord sum;
+
+    sum.xc=vector1.xc + vector2.xc;
+    sum.yc=vector1.yc + vector2.yc;
+    return(sum);
+}
+
+    //Multiplicación por un escalar
+coord multiply_vector_scalar(coord vector1,float cnt){
+
+    coord mult;
+
+    mult.xc=vector1.xc * cnt;
+    mult.yc=vector1.yc * cnt;
+    return(mult);
+}
+
+    //Magnitud del vector
+float magnitudVector(coord punto)
+{
+    float result;
+    float aux;
+
+    aux = (punto.xc * punto.xc) + (punto.yc * punto.yc);
+    result = sqrt(aux);
+
+    return result;  
+}
+
+coord vetorNormalized(coord punto)
+{
+    coord result;
+    float mag;
+    
+    //Se obtiene su magnitud:
+    mag = magnitudVector(punto);
+
+    //Se obtiene el vector normalizado
+    result.xc = punto.xc / mag;
+    result.yc = punto.yc / mag;
+
+    return result;
+}
+
+// Cálculo de 1 punto mediante campos potenciales
+coord puntosRobot(coord q_0, coord q_dest, coord q_obs, float e1, float d0, float n, float delt_0)
+{
+    coord Fatr;
+    coord Drep;
+    coord resta1;
+    coord F;
+    coord f;
+    coord q; //Punto calculado
+
+    float escAux;
+
+    //Fuerza de atracción
+    Fatr = multiply_vector_scalar(dif_vectors(q_0,q_dest),e1);
+    resta1 = dif_vectors(q_0,q_obs);
+
+    //Escalar auxiliar
+    escAux = -n * (1 / magnitudVector(dif_vectors(q_0,q_obs))) -(1 / d0) * (1 / ( magnitudVector(dif_vectors(q_0,q_obs)) * magnitudVector(dif_vectors(q_0,q_obs)) ) );
+
+    //Fuerza de repulsión (?)
+    Drep = multiply_vector_scalar(vetorNormalized(resta1),escAux);
+
+    //Fuerza:
+    F = sum_vectors(Fatr,Drep);
+    f=vetorNormalized(F);
+    q = dif_vectors(q_0,multiply_vector_scalar(f,delt_0));
+
+    return q;
+}
+
+
+/* ---------------------------------------------------------- POTENTIAL FIELDS ---------------------------------------------------------------------*/
+AdvanceAngle potential_fields(Raw observations, int dest, int intensity, float Mag_Advance, float max_angle, int num_sensors, coord coord_robot, coord coord_dest){
 
  AdvanceAngle gen_vector;
  int obs;
@@ -18,6 +104,8 @@ AdvanceAngle potential_fields(Raw observations, int dest, int intensity, float M
  float right_side=0;
  int value = 0;
  static int step=0;
+
+ //static coord obs; //Keep the coords of the obstacle according to the sensors.
 
  step++;
  printf("\n\n **************** Student Reactive Behavior %d *********************\n",step);
@@ -46,33 +134,7 @@ AdvanceAngle potential_fields(Raw observations, int dest, int intensity, float M
  obs = value;
  printf("intensity %d obstacles %d dest %d\n",intensity,obs,dest);
 
- //No obstacle
- if(obs==0){
-	 gen_vector=generate_output(FORWARD,Mag_Advance,max_angle);
-	 printf("FORWARD");
- }
-
- //Obstacle to the RIGHT
-
- else if(obs==1){
-	 gen_vector=generate_output(FORWARD,Mag_Advance,max_angle);
-         printf("FORWARD");
-
- }
-
- //Obstacle to the LEFT
-
- else if(obs==2){
-	 gen_vector=generate_output(FORWARD,Mag_Advance,max_angle);
-         printf("FORWARD");
-
- }
-
- //Obstacle in FRONT 
- else if(obs==3){
-	 gen_vector=generate_output(LEFTADVANCETWICE,Mag_Advance,max_angle);
-         printf("LEFT");
- }
+ /* ---------------------------------------- ALGORITHM ----------------------------------------------- */
  
  return gen_vector;
 
