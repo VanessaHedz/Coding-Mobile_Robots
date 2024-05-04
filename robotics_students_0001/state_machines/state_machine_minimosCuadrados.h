688d568c4f8e3dc4eb9c42e38953768b0d355cce
VanessaHedz <82928993+VanessaHedz@users.noreply.github.com>
2024-05-04 14:46:34 -0600

    Add files via upload

---
+++ robotics_students_0001/state_machines/state_machine_minimosCuadrados.h
@@ -0,0 +1,19 @@
+/********************************************************
+ *                                                      *
+ *                                                      *
+ *      state_machine_destination.h                    	*
+ *                                                      *
+ *		Jesus Savage				*
+ *		FI-UNAM					*
+ *		6-1-2015                                *
+ *                                                      *
+ ********************************************************/
+
+
+// Function to get next state
+AdvanceAngle minimosCuadrados(int dest, int intensity, int state, int *next_state,float Mag_Advance,float max_angle){
+
+ AdvanceAngle gen_vector;
+
+ return gen_vector;
+}
